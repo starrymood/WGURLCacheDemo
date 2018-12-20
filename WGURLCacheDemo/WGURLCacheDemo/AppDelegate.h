f6885b1c90be28bd2de1095eff55613ecda8c59f
yanlu <523079673@qq.com>
2018-12-20 17:04:36 +0800

    项目初始化…

---
+++ WGURLCacheDemo/WGURLCacheDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WGURLCacheDemo
+//
+//  Created by DY on 2018/12/20.
+//  Copyright © 2018 MR.F. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WGURLCacheDemo/WGURLCacheDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  WGURLCacheDemo
+//
+//  Created by DY on 2018/12/20.
+//  Copyright © 2018 MR.F. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
